0d090e3ccf19707431347715c69c38c6afa6a640
LFSSRR <92567850+lfsrrr@users.noreply.github.com>
2023-10-01 19:59:55 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const sternzeichenSelect = document.getElementById('sternzeichen');
+    const horoskopContainer = document.getElementById('horoskop-output');
+    const sternzeichenTitel = document.getElementById('sternzeichen-titel');
+
+    sternzeichenSelect.addEventListener('change', function() {
+        const ausgewaehltesSternzeichen = this.value;
+
+        fetch('horoskope.json')
+            .then(response => response.json())
+            .then(data => {
+                const horoskop = data.Horoskope.find(h => h.Sternzeichen === ausgewaehltesSternzeichen);
+
+                if (horoskop) {
+                    sternzeichenTitel.textContent = horoskop.Sternzeichen;
+
+                    horoskopContainer.innerHTML = `
+                        <p><strong>Prognose:</strong> ${horoskop.Prognose}</p>
+                        <p><strong>Glückszahl:</strong> ${horoskop.Glückszahl}</p>
+                        <p><strong>Glücksfarbe:</strong> <span class="gluecksfarbe" style="background-color:${horoskop.Glücksfarbe};"></span></p>
+                    `;
+                } else {
+                    sternzeichenTitel.textContent = "Horoskop nicht gefunden";
+                    horoskopContainer.innerHTML = "";
+                }
+            });
+    });
+});
